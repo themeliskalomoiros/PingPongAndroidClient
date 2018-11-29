3763430fb4fc1f1d5db514ee1f5033e7d48ce40a
Tomas <tomasmichael995@gmail.com>
2018-11-29 19:13:00 +0100

    setupUi() in MainActivity

--- app/src/main/java/gr/kalymnos/skemelio/pingpongandroidclient/mvc_controller/MainActivity.java
+++ app/src/main/java/gr/kalymnos/skemelio/pingpongandroidclient/mvc_controller/MainActivity.java
@@ -2,14 +2,26 @@ package gr.kalymnos.skemelio.pingpongandroidclient.mvc_controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 import gr.kalymnos.skemelio.pingpongandroidclient.R;
+import gr.kalymnos.skemelio.pingpongandroidclient.mvc_view.ToolbaredViewMvc;
+import gr.kalymnos.skemelio.pingpongandroidclient.mvc_view.screen_main.MainScreenViewMvcImp;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ToolbaredViewMvc viewMvc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen_main);
+        setupUi();
+    }
+
+    private void setupUi() {
+        viewMvc = new MainScreenViewMvcImp(LayoutInflater.from(this),null);
+        viewMvc.bindToolbarTitle("Connection Phase");
+        setSupportActionBar(viewMvc.getToolbar());
+        setContentView(viewMvc.getRootView());
     }
 }
