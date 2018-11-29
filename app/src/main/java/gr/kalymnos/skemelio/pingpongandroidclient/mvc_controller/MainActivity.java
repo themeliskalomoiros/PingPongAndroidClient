06a4b4597afcdcdc8b121f195b45a6474cd10f64
Tomas <tomasmichael995@gmail.com>
2018-11-29 18:48:01 +0100

    Created screen_main layout

--- app/src/main/java/gr/kalymnos/skemelio/pingpongandroidclient/mvc_controller/MainActivity.java
+++ app/src/main/java/gr/kalymnos/skemelio/pingpongandroidclient/mvc_controller/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen_ping_pong);
+        setContentView(R.layout.screen_main);
     }
 }
