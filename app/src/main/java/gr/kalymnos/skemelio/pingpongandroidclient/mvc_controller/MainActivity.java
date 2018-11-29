5f91d2a9f6884e59d401e3dbc8428655b14fbb76
Tomas <tomasmichael995@gmail.com>
2018-11-29 19:47:09 +0100

    MainActivity implements ConnectionFragment.OnConnectClickListener, PingPongFragment.OnSendClickListener

--- app/src/main/java/gr/kalymnos/skemelio/pingpongandroidclient/mvc_controller/MainActivity.java
+++ app/src/main/java/gr/kalymnos/skemelio/pingpongandroidclient/mvc_controller/MainActivity.java
@@ -6,11 +6,13 @@ import android.view.LayoutInflater;
 
 import gr.kalymnos.skemelio.pingpongandroidclient.R;
 import gr.kalymnos.skemelio.pingpongandroidclient.mvc_view.ToolbaredViewMvc;
+import gr.kalymnos.skemelio.pingpongandroidclient.mvc_view.screen_main.MainScreenViewMvc;
 import gr.kalymnos.skemelio.pingpongandroidclient.mvc_view.screen_main.MainScreenViewMvcImp;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ConnectionFragment.OnConnectClickListener, PingPongFragment.OnSendClickListener {
 
-    private ToolbaredViewMvc viewMvc;
+    private MainScreenViewMvc viewMvc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +21,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupUi() {
-        viewMvc = new MainScreenViewMvcImp(LayoutInflater.from(this),null);
+        viewMvc = new MainScreenViewMvcImp(LayoutInflater.from(this), null);
         viewMvc.bindToolbarTitle("Connection Phase");
         setSupportActionBar(viewMvc.getToolbar());
         setContentView(viewMvc.getRootView());
+        getSupportFragmentManager().beginTransaction()
+                .replace(viewMvc.getFragmentContainerId(), new ConnectionFragment())
+                .commit();
+    }
+
+    @Override
+    public void onConnectClicked(String host, int port) {
+
+    }
+
+    @Override
+    public void onSendClicked() {
+
     }
 }
