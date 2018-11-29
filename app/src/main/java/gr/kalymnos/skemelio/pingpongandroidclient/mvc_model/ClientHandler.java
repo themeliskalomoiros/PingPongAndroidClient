b481da95fb0b2c4ab1b018bf3e5d210e974e50e5
Tomas <tomasmichael995@gmail.com>
2018-11-29 18:37:30 +0100

    Created ClientHandler's sendReceivedPingMsg()

--- app/src/main/java/gr/kalymnos/skemelio/pingpongandroidclient/mvc_model/ClientHandler.java
+++ app/src/main/java/gr/kalymnos/skemelio/pingpongandroidclient/mvc_model/ClientHandler.java
@@ -5,18 +5,24 @@ import android.os.Looper;
 import android.os.Message;
 
 /*
-*  This handler will update the UI thread about Client's messages.
-* */
+ *  This handler will update the UI thread about Client's messages.
+ * */
 
 public class ClientHandler extends Handler {
 
-    public ClientHandler(){
-        super(Looper.getMainLooper());
+    public ClientHandler(Callback callback) {
+        super(callback);
     }
 
-    public void sendConnectionErrorMsg(){
+    public void sendConnectionErrorMsg() {
         Message msg = obtainMessage();
         msg.what = ClientThread.CONNECTION_ERROR;
+        sendMessage(msg);
+    }
+
+    public void sendReceivedPingMsg() {
+        Message msg = obtainMessage();
+        msg.what = ClientThread.RECEIVED_PING;
         sendMessage(msg);
     }
 }
